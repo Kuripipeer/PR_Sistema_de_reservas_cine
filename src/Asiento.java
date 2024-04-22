05acffebd2427b4d9124ba8c01f65996307e3cbf
Ángel Becerra <angelmirocu@gmail.com>
2024-04-22 15:18:34 -0600

    Update class Asiento.java and Asiento.class

--- src/Asiento.java
+++ src/Asiento.java
@@ -1,3 +1,29 @@
 public class Asiento {
-    
+
+    private String IdSala;
+    private boolean Estado = false;
+
+    public Asiento(String IdSala) {
+        this.IdSala = IdSala;
+    }
+
+    public String getIdSala() {
+        return IdSala;
+    }
+
+    public void setIdSala(String IdSala) {
+        this.IdSala = IdSala;
+    }
+
+    public void LiberarAsiento() {
+        this.Estado = false;
+    }
+
+    public void ReservarAsiento() {
+        this.Estado = true;
+    }
+
+    public void MostrarEstado() {
+        System.out.println("El asiento esta " + (Estado ? "reservado" : "libre") + " en la sala " + IdSala + ".");
+    }
 }
